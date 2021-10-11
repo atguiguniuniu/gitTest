9ef160ec035797223874cc2dc0b0f6f41d234202
xiaoneng <xiaoneng@yz.com>
2021-10-11 16:00:10 +0800

    测试第二次提交

--- src/main/java/com/yunzhan/test/Test01.java
+++ src/main/java/com/yunzhan/test/Test01.java
@@ -2,6 +2,6 @@ package com.yunzhan.test;
 
 public class Test01 {
     public static void main(String[] args) {
-        System.out.println("Test01");
+        System.out.println("Test02");
     }
 }
